8314e476b8b721413325da14402aa01490d343ba
kumardurgesh278 <70564546+kumardurgesh278@users.noreply.github.com>
2021-05-04 15:08:26 +0530

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("this code is to be uploaded on github...");
+    }
+}
