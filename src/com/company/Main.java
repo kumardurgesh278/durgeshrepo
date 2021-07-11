34b83a7fb66cfd76723b4358eca8bd92e69ca842
kumardurgesh278 <70564546+kumardurgesh278@users.noreply.github.com>
2021-07-11 05:44:03 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("this code is to be uploaded on github....");
+        System.out.println("this code is to be uploaded on github.....");
     }
 }
